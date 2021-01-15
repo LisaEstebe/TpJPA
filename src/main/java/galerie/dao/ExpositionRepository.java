de315a56915a05cc3dfd88d3681e6359897b99b1
lisa <lisa.estebe@gmail.com>
2021-01-15 15:25:20 +0100

    Repository

---
+++ src/main/java/galerie/dao/ExpositionRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galerie.dao;
+
+/**
+ *
+ * @author Lisa
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import galerie.entity.Galerie;
+
+// This will be AUTO IMPLEMENTED by Spring 
+
+public interface ExpositionRepository extends JpaRepository<Galerie, Integer>{
+    
+}
